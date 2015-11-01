c51f040f39be3291a26f2480bc2e21dacd079aa0
prm10 <353345737@qq.com>
2015-11-01 10:02:07 +0800

    a

--- shell.sh
+++ shell.sh
@@ -3,7 +3,7 @@ scp ~/prm14-1.0.0.jar root@hadoopserver3:/home
 ssh root\@hadoopserver3
 
 cd /usr/hdp/2.3.2.0-2950/spark
-./bin/spark-submit --class "datrain.item_based_CF" --master spark://192.168.1.50:7077 --executor-memory 20G --total-executor-cores 24 /home/prm14-1.0.0.jar /tmp/prm_user1
+./bin/spark-submit --class "datrain.item_based_CF" --master spark://192.168.1.50:7077 --executor-memory 20G --total-executor-cores 40 /home/prm14-1.0.0.jar /tmp/prm_user1
 
 su hdfs
 hadoop fs -rm -r /tmp/prm_output1
@@ -15,5 +15,8 @@ spark-submit --class "datrain.item_based_CF" --master local[4] prm14-1.0.0.jar b
 
 http://sparkbj38080.tunnel.yottabig.com:8000/
 
+scp /data/tian root@hadoopserver1:/home
 
+共有10000个用户、2908334个商品的11600432条浏览行为
 
+groupByKey会造成大量的数据搬移，效率不高
