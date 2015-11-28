8b59c96c7c7230fbb008899c4addcf0566d7da02
prm10 <353345737@qq.com>
2015-11-29 00:26:23 +0800

    a

--- first_spark_project/src/main/java/datrain/item_based_CF.java
+++ first_spark_project/src/main/java/datrain/item_based_CF.java
@@ -18,99 +18,104 @@ import java.util.*;
 import java.util.regex.Pattern;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+
 import org.apache.spark.sql.RowFactory;
 
 /**
  * Created by prm14 on 2015/10/20.
  */
 
-public class item_based_CF {
+public class item_based_CF implements Serializable {
+    private SQLContext sqlContext = null;
+    private String inputFileName = null;
+    private String outputFileName = null;
+    private String inputStructType = "user_id:String;item_id:String;behavior_type:String";
+    private String outputStructType = "item_id:String;item_list:String";
+    private DataFrame inputDF = null;
+    private DataFrame outputDF = null;
+    private Long maxBehaviorTimes = null;
 
-//    public static class InputClass implements Serializable {
-//        private Long user_id;
-//        private Long item_id;
-//        private Long behavior_type;
-//
-//        InputClass(Long s1, Long s2, Long s3) {
-//            user_id = s1;
-//            item_id = s2;
-//            behavior_type=s3;
-//        }
-//    }
-//
-//    public static class OutputClass implements Serializable {
-//        public Long item_id;
-//        public String item_list;
-//
-//        OutputClass(Long i1, String list) {
-//            item_id = i1;
-//            item_list = list;
-//        }
-//    }
+    public void setInputFileName(String s) {
+        inputFileName = s;
+    }
 
-    public static StructType gernerateStructType(String s){
-        String[] kvs=s.split(";");
+    public void setOutputFileName(String s) {
+        outputFileName = s;
+    }
+
+    public void setInputStructType(String s) {
+        inputStructType = s;
+    }
+
+    public void setOutputStructType(String s) {
+        outputStructType = s;
+    }
+
+    public void setInputDF(JavaRDD<Row> s) {
+        inputDF = sqlContext.createDataFrame(s, generateStructType(inputStructType));
+    }
+
+    public void setOutputDF(JavaRDD<Row> s) {
+        outputDF = sqlContext.createDataFrame(s, generateStructType(outputStructType));
+    }
+
+    public void setMaxBehaviorTimes(Long s) {
+        maxBehaviorTimes = s;
+    }
+
+    public StructType generateStructType(String s) {
+        String[] kvs = s.split(";");
         List<StructField> fields = new ArrayList<StructField>();
-        for(String x:kvs){
-            String[] kv=x.split(":");
-            String name=kv[0];
-            String dataType=kv[1];
-            if(dataType.equals("String")){
-                fields.add(DataTypes.createStructField(name,DataTypes.StringType,true));
+        for (String x : kvs) {
+            String[] kv = x.split(":");
+            String name = kv[0];
+            String dataType = kv[1];
+            if (dataType.equals("String")) {
+                fields.add(DataTypes.createStructField(name, DataTypes.StringType, true));
             }
-            if(dataType.equals("Long")){
-                fields.add(DataTypes.createStructField(name,DataTypes.LongType,true));
+            if (dataType.equals("Long")) {
+                fields.add(DataTypes.createStructField(name, DataTypes.LongType, true));
             }
         }
         return DataTypes.createStructType(fields);
     }
-    public static DataFrame getData(JavaSparkContext ctx,String way,String[] args,String structType){
-        DataFrame inputDF=null;
-        SQLContext sqlContext=new SQLContext(ctx);
-        if(way.equals("hive")) {
+
+    public void getInputDF(JavaSparkContext ctx, String way) {
+        if (way.equals("hive")) {
             HiveContext hiveCtx = new HiveContext(ctx.sc());
-            DataFrame df = hiveCtx.sql("select user_id,item_id,behavior_type from tmalldb.user_info");//读取数据，存入dataframe
+            DataFrame df = hiveCtx.sql("select user_id,item_id,behavior_type from " + inputFileName);//读取数据，存入dataframe
             df.printSchema();
-            inputDF = sqlContext.createDataFrame(df.toJavaRDD(),gernerateStructType(structType));
-        }
-        else{
-            JavaRDD<Row> lines0=ctx.textFile(args[0], 1).map(new Function<String, Row>() {
+            setInputDF(df.toJavaRDD());
+        } else {
+            JavaRDD<Row> lines0 = ctx.textFile(inputFileName, 1).map(new Function<String, Row>() {
                 @Override
                 public Row call(String s) throws Exception {
-                    String [] info=s.split(",");
-                    return RowFactory.create(info[0],info[1],info[2]);
+                    String[] info = s.split(",");
+                    return RowFactory.create(info[0], info[1], info[2]);
                 }
             });
-            inputDF = sqlContext.createDataFrame(lines0, gernerateStructType(structType));
+            setInputDF(lines0);
         }
         inputDF.printSchema();
         System.out.println("总共读入" + inputDF.count() + "行数据");
-        return inputDF;
     }
 
-    public static void saveData(JavaSparkContext ctx,JavaRDD<Row> outfile,String structType){
+    public void saveOutputDF(JavaSparkContext ctx) {
         // 存入hive
         HiveContext hiveCtx = new HiveContext(ctx.sc());
-        hiveCtx.sql("create external table if not exists tmalldb.prm14_result(item_id bigint,item_list string) partitioned by (ds string)");
-        DataFrame result_df=hiveCtx.createDataFrame(outfile, gernerateStructType(structType));
-        result_df.printSchema();
-        result_df.registerTempTable("temp_table1");
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        String ds=df.format(new Date());
-        hiveCtx.sql("insert overwrite table tmalldb.prm14_result partition(ds='"+ds+"') select * from temp_table1");
+        hiveCtx.sql("create external table if not exists " + outputFileName + "(item_id string,item_list string) partitioned by (ds string)");
+        DataFrame df = hiveCtx.createDataFrame(outputDF.toJavaRDD(), generateStructType(outputStructType));
+        df.printSchema();
+        df.registerTempTable("temp_table1");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String ds = sdf.format(new Date());
+        hiveCtx.sql("insert overwrite table " + outputFileName + " partition(ds='" + ds + "') select * from temp_table1");
     }
 
-    public static void main(String[] args) throws Exception {
-        if (args.length < 1) {
-            System.err.println("Usage: item_based_CF <file>");
-        }
-
-        SparkConf sparkConf = new SparkConf().setAppName("prm_item_based_CF");
-        sparkConf.set("spark.ui.port", "5555");
-        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-        String structType="user_id:String;item_id:String;behavior_type:String";
-        DataFrame inputDF=getData(ctx,"hive",args,structType);
-        JavaPairRDD<Long, Long> user_behavior=inputDF.toJavaRDD().filter(new Function<Row, Boolean>() {
+    public void IBCF(JavaSparkContext ctx) {
+        sqlContext = new SQLContext(ctx);
+        getInputDF(ctx, "hive");
+        JavaPairRDD<Long, Long> user_behavior = inputDF.toJavaRDD().filter(new Function<Row, Boolean>() {
             @Override
             public Boolean call(Row row) throws Exception {
                 return Long.parseLong(row.getString(2)) == 1L;
@@ -138,26 +143,6 @@ public class item_based_CF {
         ctx.broadcast(user_times);
 
         System.out.println("共有用户" + user_times.size() + "个。");
-
-        //每个item的行为次数
-//        final Map<Long, Long> item_num = user_behavior.mapToPair(new PairFunction<Tuple2<Long, Long>, Long, Long>() {
-//            @Override
-//            public Tuple2<Long, Long> call(Tuple2<Long, Long> s) throws Exception {
-//                return new Tuple2<Long, Long>(s._2, 1L);
-//            }
-//        }).reduceByKey(new Function2<Long, Long, Long>() {
-//            @Override
-//            public Long call(Long l1, Long l2) throws Exception {
-//                return l1 + l2;
-//            }
-//        }).filter(new Function<Tuple2<Long, Long>, Boolean>() {
-//            @Override
-//            public Boolean call(Tuple2<Long, Long> s) throws Exception {
-//                return s._2 > 50L;
-//            }
-//        }).collectAsMap();
-//
-//        System.out.println("共有有效商品" + item_num.size() + "个。");
 
         JavaPairRDD<Tuple2<Long, Long>, Long> ui_times = user_behavior.mapToPair(new PairFunction<Tuple2<Long, Long>, Tuple2<Long, Long>, Long>() {
             @Override
@@ -187,7 +172,7 @@ public class item_based_CF {
         }).filter(new Function<Tuple2<Long, Vector<Tuple2<Long, Long>>>, Boolean>() {
             @Override//去除行为过多的用户
             public Boolean call(Tuple2<Long, Vector<Tuple2<Long, Long>>> s) throws Exception {
-                return s._2().size() < 100;
+                return s._2().size() < maxBehaviorTimes;
             }
         });//.repartition(200);
         System.out.println("形成了" + user_behaviors.count() + "个用户的行为数据。");
@@ -269,23 +254,33 @@ public class item_based_CF {
                     item_entry = heap.result[i];
                     item_list += ";" + item_entry.key + ":" + format.format(item_entry.value);
                 }
-                return RowFactory.create(item1, item_list);
+                return RowFactory.create(String.valueOf(item1), item_list);
             }
         });
         System.out.println("生成推荐列表共" + outfile.count() + "个");
 
-        structType="item_id:Long;item_list:String";
-        saveData(ctx,outfile,structType);
+        setOutputDF(outfile);
+        saveOutputDF(ctx);
+        int i = 0;
+        for (Row show : outfile.collect()) {
+            System.out.println(show.getString(0));
+            System.out.println("{" + show.getString(1) + "}");
+            i = i + 1;
+            if (i > 20) {
+                break;
+            }
+        }
+    }
 
-//        int i = 0;
-//        for (Row show : outfile.collect()) {
-//            System.out.println(show.getLong(0));
-//            System.out.println("{" + show.getString(1) + "}");
-//            i = i + 1;
-//            if (i > 100) {
-//                break;
-//            }
-//        }
+    public static void main(String[] args) throws Exception {
+        SparkConf sparkConf = new SparkConf().setAppName("prm_item_based_CF");
+        sparkConf.set("spark.ui.port", "5555");
+        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        item_based_CF ibcf = new item_based_CF();
+        ibcf.setInputFileName("tmalldb.user_info");
+        ibcf.setOutputFileName("tmalldb.prm_cf");
+        ibcf.setMaxBehaviorTimes(100L);
+        ibcf.IBCF(ctx);
         ctx.stop();
     }
 }
